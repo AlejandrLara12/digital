73922b3acf88638ef7f1216c158cdf6bd3c9e308
Ricardo Lara <alejandro.elara@live.com>
2021-03-08 11:56:22 -0600

    adds nuxt link to each rule

--- components/Rule.vue
+++ components/Rule.vue
@@ -9,31 +9,25 @@
                 "
             >
                 <!-- bg-gradient-to-r hover:from-blue-500 hover:to-indigo-400 -->
-                <dl
-                    class="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center"
-                >
+                <dl class="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
                     <div>
                         <dt class="sr-only">Formula</dt>
-                        <dd
-                            class="group-hover:text-white leading-6 font-medium text-black"
-                        >
+                        <dd class="group-hover:text-white leading-6 font-medium text-black">
                         {{ formula }}
                         </dd>
                     </div>
                     <div>
                         <dt class="sr-only">Category</dt>
-                        <dd
-                            class="group-hover:text-blue-200 text-sm font-medium sm:mb-4 lg:mb-0 xl:mb-4"
-                        >
+                        <dd class="group-hover:text-blue-200 text-sm font-medium sm:mb-4 lg:mb-0 xl:mb-4">
                             category
                         </dd>
                     </div>
                     <div class="col-start-2 row-start-1 row-end-3">
                         <dt class="sr-only">Users</dt>
-                        <dd
-                            class="flex justify-end sm:justify-start lg:justify-end xl:justify-start -space-x-2"
-                        >
-                            stuff
+                        <dd class="flex justify-end sm:justify-start lg:justify-end xl:justify-start -space-x-2">
+                            <NuxtLink :to="'/algebra-boleana/'+id" >
+                                Detalle
+                            </NuxtLink>
                         </dd>
                     </div>
                 </dl>
@@ -48,7 +42,10 @@ export default {
     props: {
         formula: {
             type: String,
-            required: false,
+            required: true,
+        },
+        id: {
+            type: String,
         },
     },
     components: {},
