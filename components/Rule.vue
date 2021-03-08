7bf7b6edd637fa91dfcd01a6919fb2693d940af7
Ricardo Lara <alejandro.elara@live.com>
2021-03-08 00:41:45 -0600

    adds rules page

---
+++ components/Rule.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="rule-container">
+        <li>
+            <div
+                class="
+                    group block rounded-lg p-4 border border-gray-200
+                    hover:border-transparent hover:shadow-lg 
+                    hover:bg-blue-500
+                "
+            >
+                <!-- bg-gradient-to-r hover:from-blue-500 hover:to-indigo-400 -->
+                <dl
+                    class="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center"
+                >
+                    <div>
+                        <dt class="sr-only">Formula</dt>
+                        <dd
+                            class="group-hover:text-white leading-6 font-medium text-black"
+                        >
+                        {{ formula }}
+                        </dd>
+                    </div>
+                    <div>
+                        <dt class="sr-only">Category</dt>
+                        <dd
+                            class="group-hover:text-blue-200 text-sm font-medium sm:mb-4 lg:mb-0 xl:mb-4"
+                        >
+                            category
+                        </dd>
+                    </div>
+                    <div class="col-start-2 row-start-1 row-end-3">
+                        <dt class="sr-only">Users</dt>
+                        <dd
+                            class="flex justify-end sm:justify-start lg:justify-end xl:justify-start -space-x-2"
+                        >
+                            stuff
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Rule',
+    props: {
+        formula: {
+            type: String,
+            required: false,
+        },
+    },
+    components: {},
+};
+</script>
+<style></style>
---
+++ pages/algebra-boleana.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <section
+            class="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4"
+        >
+            <header class="flex items-center justify-between">
+                <h2 class="text-lg leading-6 font-medium text-black">Reglas de Algebra Boleana</h2>
+            </header>
+            <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4">
+                <Rule v-for="rule in rules" :key="rule.id" v-bind="rule" />
+            </ul>
+        </section>
+    </div>
+</template>
+
+<script>
+import Rule from '../components/Rule.vue';
+
+export default {
+    layout: 'default',
+    components: {
+        Rule,
+    },
+    data: () => ({
+        rules: [
+            { id: 1, formula: 'A + 0 = A' },
+            { id: 2, formula: 'A + 1 = 1' },
+            { id: 3, formula: 'A * 0 = 0' },
+            { id: 4, formula: 'A * 1 = A' },
+            { id: 5, formula: 'A + A = A' },
+            { id: 6, formula: 'A + Ā = 1' },
+            { id: 7, formula: 'A * A = A' },
+            { id: 8, formula: 'A * ¬A = 0' },
+            { id: 9, formula: '¬¬A = A' },
+            { id: 10, formula: 'A + AB = A' },
+            { id: 11, formula: 'A + ¬AB = A + B' },
+            { id: 12, formula: '(A + B)(A + C) = A + BC' },
+        ],
+        items: [
+            { 
+                url: 'google.com', title: 'API Integration', category: 'Engineering', 
+                users: [
+                    { name: 'Lara', avatar: 'https://via.placeholder.com/150/92c952' },
+                    { name: 'Lara', avatar: 'https://via.placeholder.com/150/771796' },
+                    { name: 'Lara', avatar: 'https://via.placeholder.com/150/24f355' },
+                ] 
+            },
+            { 
+                url: 'google.com', title: 'Onboarding Emails', category: 'Customer Success', 
+                users: [
+                    { name: 'Lara', avatar: 'https://via.placeholder.com/150/24f355' },
+                ] 
+            },
+        ]
+    }),
+};
+</script>
+
+<style></style>
